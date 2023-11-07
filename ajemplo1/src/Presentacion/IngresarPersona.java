7a0b2f86fbd527017be831b2d1118e4b732e1025
lola <lolita@gmail.com>
2023-11-07 15:30:49 -0500

    ok2.0

--- ajemplo1/src/Presentacion/IngresarPersona.java
+++ ajemplo1/src/Presentacion/IngresarPersona.java
@@ -15,24 +15,39 @@ public class IngresarPersona extends JFrame{
     private JButton btnGuardar;
     private JComboBox cmbIdioma;
     private JLabel lblIdioma;
+    private JCheckBox chbTerminos;
+    private JLabel lblGenero;
+    private JRadioButton rbtMasculino;
+    private JRadioButton rbtFemenino;
+    private JRadioButton rbtOtro;
+    private JRadioButton rbtNoIndicado;
 
     public IngresarPersona(){
-       this.setTitle("Manitas sudadas(Ingresar_Personas)");
+       this.setTitle("Manitas sudadas(Ingresar Personas)");
        this.setContentPane(pnlPrincipal);
-       this.setSize(400,300);
+       this.setSize(500,300);
        this.setVisible(true);
+       this.setLocationRelativeTo(null);
         btnGuardar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String codigo =txtCodigo.getText();
                 String nombre = txtNombre.getText();
                 String apellido = txtApellido.getText();
+                String idioma =cmbIdioma.getSelectedItem().toString();
+                String aceptoTerminos = (chbTerminos.isSelected())?"Si":"No"; //Necesito hacer una igualacion a un valor a una variable
+                String genero = (rbtMasculino.isSelected())?"Masculino":
+                        (rbtFemenino.isSelected())? "Femenino":
+                                (rbtOtro.isSelected())? "Otro":
+                                        (rbtNoIndicado.isSelected())? "No indicado":"";
 
                 String mensaje = "Tus datos son: \n" +
-                        "Codigo:" + codigo + "\n" +
-                        "Nombre:" + nombre + "\n" +
-                        "Apellido:" + apellido;
-
+                        "Codigo: " + codigo + "\n" +
+                        "Nombre: " + nombre + "\n" +
+                        "Apellido: " + apellido + "\n" +
+                        "Idioma: " + idioma + "\n" +
+                        "Genero: " + genero + "\n" +
+                        "Aceptò Terminos: "+ aceptoTerminos;
 
                 JOptionPane.showMessageDialog(btnGuardar, mensaje);
             }
--- ajemplo1/src/Principal.java
+++ ajemplo1/src/Principal.java
@@ -1,6 +1,6 @@
 import Presentacion.IngresarPersona;
 
-public class Principal {
+public class   Principal {
     public static void main (String[] args){
         IngresarPersona  ventana = new IngresarPersona ();
     }
